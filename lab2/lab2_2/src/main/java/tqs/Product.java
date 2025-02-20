567681825925d6d0ad928855705799286780de92
tiagoflopes <tiagoflopes@ua.pt>
2025-02-20 17:59:25 +0000

    lab2_2 basic structure

---
+++ lab2/lab2_2/src/main/java/tqs/Product.java
@@ -0,0 +1,73 @@
+package tqs;
+
+public class Product {
+
+    private Integer id;
+    private String image;
+    private String description;
+    private Double price;
+    private String title;
+    private String category;
+
+    public Product() {
+
+    }
+
+    public Product(Integer id, String image, String description, Double price, String title, String category) {
+        this.id = id;
+        this.image = image;
+        this.description = description;
+        this.price = price;
+        this.title = title;
+        this.category = category;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+}
---
+++ lab2/lab2_2/src/main/java/tqs/ProductFinderService.java
@@ -0,0 +1,5 @@
+package tqs;
+
+public class ProductFinderService {
+
+}
