9f6ff293e1a9c04dd52a87a09bceac6fc0006547
tiagoflopes <tiagoflopes@ua.pt>
2025-03-08 14:53:58 +0000

    lab4_2

---
+++ lab4/lab4_2/src/main/java/tqs/lab4_2/Lab42Application.java
@@ -0,0 +1,13 @@
+package tqs.lab4_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab42Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab42Application.class, args);
+    }
+
+}
---
+++ lab4/lab4_2/src/test/java/tqs/lab4_2/Lab42ApplicationTests.java
@@ -0,0 +1,13 @@
+package tqs.lab4_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab42ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
