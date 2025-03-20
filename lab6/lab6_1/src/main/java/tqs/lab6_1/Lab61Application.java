17c096ef7fff3f3666df1e3193f515b669520320
tiagoflopes <tiagoflopes@ua.pt>
2025-03-20 17:57:19 +0000

    lab6_1 tests in wrong order

---
+++ lab6/lab6_1/src/main/java/tqs/lab6_1/Lab61Application.java
@@ -0,0 +1,13 @@
+package tqs.lab6_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab61Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab61Application.class, args);
+    }
+
+}
